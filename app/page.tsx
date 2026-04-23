e87261562ddf901655b5759371e9de9d83e9caac
yohana0958 <yohana.souz4@gmail.com>
2026-04-23 10:46:40 -0300

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import "./home.css";
+
+export default function Home(){
+  
+  return(
+    
+    <div className="cont">
+       <h1 className="titulo">Minhas Férias 🌴✨</h1>
+      Passei bastante tempo em chácaras com a minha família, aproveitando aquele clima de paz, natureza e descanso de verdade. Também curti muito com a minha filha, criando memórias especiais e aproveitando cada momento ao lado dela, o que foi, sem dúvida, a melhor parte.
+    </div>
+  )
+}
